b25de7e4f44cd548efd9b418e9368fd579ef948a
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-13 16:19:48 +0100

    Random number generator and comparison to find the largest

--- dadi/main.js
+++ dadi/main.js
@@ -1,3 +1,14 @@
-/* alert("Tira il dado premendo ok") */
+alert("Tira il dado premendo ok")
 let userNumber = Math.floor(Math.random() * 6) + 1;
 console.log(userNumber)
+alert(`Il tuo numero è ${userNumber} ora premi ok per far tirare il pc`)
+let pcNumber = Math.floor(Math.random() * 6) + 1;
+console.log(pcNumber)
+alert(`Il  numero del pc ${pcNumber} ora premi ok per sapere chi ha vinto`)
+if (userNumber > userNumber) {
+    console.log("Complimenti hai vinto!")
+
+} else {
+    console.log("Ritenta sarai più fortunato!")
+    alert("Ritenta sarai più fortunato!")
+}
